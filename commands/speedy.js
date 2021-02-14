6f069b477fb297e8ae11d1d426cf05034f68972b
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 11:14:56 -0800

    Cleanup

--- commands/speedy.js
+++ commands/speedy.js
@@ -5,6 +5,5 @@ module.exports = {
         const fs = require('fs');
         var textFile = fs.readFileSync('resources/speedy.txt', {"encoding": "utf-8"});
         message.channel.send(textFile)
-        //message.reply(`Hello!  I can help by telling you stuff. Here are some things I can do:\n\n Want the link to the logs? Type !logs\n Want a real Adventure? Type !adventure\n Why not flip a mini-wheat? Type !miniwheat\n Want something else? Ask Doolan.`);
     },
 };
