d8a3e3552cca8bccd927c689428424163e4edcb7
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 14:53:10 -0800

    Tweak to speedy.js

--- commands/speedy.js
+++ commands/speedy.js
@@ -2,10 +2,6 @@ module.exports = {
     name: 'speedy',
     description: 'Speedy!',
     execute(message, args) {
-        const fs = require('fs');
-        var textFile = fs.readFileSync('resources/speedy.txt', {
-            "encoding": "utf-8"
-        });
         message.reply(`
             Hello!  I can help by telling you stuff.  Here are some things I can do:\n
             __Remember: All commands start with a **!**__\n
