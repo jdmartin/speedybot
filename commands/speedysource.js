ab6c96f8e64f4ceb2dc65b5297d90ee94a9c1310
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 11:31:03 -0800

    Commented index.js

---
+++ commands/speedysource.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'miniwheat',
+	description: 'Flip a Mini-Wheat!',
+	execute(message, args) {
+        message.reply(`My source code is here: https://github.com/jdmartin/speedybot`);
+	},
+};
