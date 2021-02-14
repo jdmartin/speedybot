10baabe65bd9b352833b0b89a293abb025ef3554
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 14:27:09 -0800

    Testing a change to speedy.txt

--- commands/speedy.js
+++ commands/speedy.js
@@ -6,6 +6,14 @@ module.exports = {
         var textFile = fs.readFileSync('resources/speedy.txt', {
             "encoding": "utf-8"
         });
-        message.channel.send(textFile)
+        message.reply(`
+        Hello!  I can help by telling you stuff.\n
+        Here are some things I can do:\n
+        !adventure:\t\tWant a real Adventure?\n
+        !logs:\t\tWant the link to the logs?\n
+        !miniwheat:\t\tWhy not flip a mini-wheat?\n
+        !speedysource:\t\tView my source code.\n\n
+        Want something else? Ask Doolan.\n
+        ~ 🐢`);
     },
 };
