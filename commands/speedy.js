c33d9f03e286bea2a706d84fbd0223623f34a88e
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 11:10:15 -0800

    Load Speedy from file

--- commands/speedy.js
+++ commands/speedy.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Speedy!',
     execute(message, args) {
         var textFile = fs.readFileSync('../resources/speedy.txt', {"encoding": "utf-8"});
-        message.channel.send(textFile)
+        message.reply.send(textFile)
         //message.reply(`Hello!  I can help by telling you stuff. Here are some things I can do:\n\n Want the link to the logs? Type !logs\n Want a real Adventure? Type !adventure\n Why not flip a mini-wheat? Type !miniwheat\n Want something else? Ask Doolan.`);
     },
 };
