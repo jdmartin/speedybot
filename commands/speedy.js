e965ecf55bfa98c945b363cd87ae7c0fb6528eef
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 11:13:27 -0800

    Fixed file path

--- commands/speedy.js
+++ commands/speedy.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Speedy!',
     execute(message, args) {
         const fs = require('fs');
-        var textFile = fs.readFileSync('../resources/speedy.txt', {"encoding": "utf-8"});
+        var textFile = fs.readFileSync('resources/speedy.txt', {"encoding": "utf-8"});
         message.channel.send(textFile)
         //message.reply(`Hello!  I can help by telling you stuff. Here are some things I can do:\n\n Want the link to the logs? Type !logs\n Want a real Adventure? Type !adventure\n Why not flip a mini-wheat? Type !miniwheat\n Want something else? Ask Doolan.`);
     },
