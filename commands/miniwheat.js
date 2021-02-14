14c54c056a6c9bd14e84b37fc5c631b404355a46
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 14:39:59 -0800

    Tweak to miniwheat output

--- commands/miniwheat.js
+++ commands/miniwheat.js
@@ -4,6 +4,6 @@ module.exports = {
 	execute(message, args) {
 		let outcomes = ["frosted", "plain"];
 		let outcomesIndex = Math.round(Math.random());
-		message.reply(`Your mini-wheat lands on the ${outcomes[outcomesIndex]} side!`);
+		message.reply(`Your mini-wheat lands on the **${outcomes[outcomesIndex]}** side!`);
 	},
 };
