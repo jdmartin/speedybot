0464ef01ad17ece1338f7df148277433109a9b3c
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 14:44:30 -0800

    Tweak to speedy.js

--- commands/speedy.js
+++ commands/speedy.js
@@ -6,6 +6,16 @@ module.exports = {
         var textFile = fs.readFileSync('resources/speedy.txt', {
             "encoding": "utf-8"
         });
-        message.channel.send(textFile)
+        message.reply(`
+            Hello!  I can help by telling you stuff.\n
+            Here are some things I can do:\n\n
+            All commands start with a **!**\n
+            **adventure**: Want a real Adventure?\n
+            **logs**: Want the link to the logs?\n
+            **miniwheat**: Why not flip a mini-wheat?\n
+            **speedy**: Displays this help message.\n
+            **speedysource**: View my source code.\n\n
+            Want something else? Ask Doolan. ~~ 🐢
+        `)
     },
 };
