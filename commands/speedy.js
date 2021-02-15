4cf603fb29b711dddff602a32edfd6ba43e1121b
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 17:12:12 -0800

    Revert changes to speedy.js

--- commands/speedy.js
+++ commands/speedy.js
@@ -2,8 +2,7 @@ module.exports = {
     name: 'speedy',
     description: 'Speedy!',
     execute(message, args) {
-        if (message.channel.type === 'dm') {
-            message.reply(`Hello!  Here are some things I can do (commands work here or in chat):\n
+        message.member.send(`Hello!  Here are some things I can do (commands work here or in chat):\n
             __Remember: All commands start with a **!**__\n
             **adventure**: Want a real Adventure?
             **logs**: Want the link to the logs?
@@ -11,17 +10,6 @@ module.exports = {
             **rules**: Read the Raid Rules!
             **speedysource**: View my source code.\n
             Want something else? Ask Doolan. ~~ 🐢
-        `)},
-        if (message.channel.type !== 'dm') {
-        message.send(`Hello!  Here are some things I can do (commands work here or in chat):\n
-        __Remember: All commands start with a **!**__\n
-        **adventure**: Want a real Adventure?
-        **logs**: Want the link to the logs?
-        **miniwheat**: Why not flip a mini-wheat?
-        **rules**: Read the Raid Rules!
-        **speedysource**: View my source code.\n
-        Want something else? Ask Doolan. ~~ 🐢
-    `)
-        }
+        `)
     },
 };
