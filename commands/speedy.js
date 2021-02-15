be41bbb6b2de0118404a84261896050ebea51c1a
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 17:08:54 -0800

    Tweak speedy.js message

--- commands/speedy.js
+++ commands/speedy.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'speedy',
     description: 'Speedy!',
     execute(message, args) {
-        if (msg.content.startsWith("!dm")) {
-            message.member.send(`Hello!  Here are some things I can do (commands work here or in chat):\n
+        if (message.channel.type === 'dm') {
+            message.reply(`Hello!  Here are some things I can do (commands work here or in chat):\n
             __Remember: All commands start with a **!**__\n
             **adventure**: Want a real Adventure?
             **logs**: Want the link to the logs?
@@ -11,7 +11,17 @@ module.exports = {
             **rules**: Read the Raid Rules!
             **speedysource**: View my source code.\n
             Want something else? Ask Doolan. ~~ 🐢
-        `)
+        `)},
+        if (message.channel.type !== 'dm') {
+        message.send(`Hello!  Here are some things I can do (commands work here or in chat):\n
+        __Remember: All commands start with a **!**__\n
+        **adventure**: Want a real Adventure?
+        **logs**: Want the link to the logs?
+        **miniwheat**: Why not flip a mini-wheat?
+        **rules**: Read the Raid Rules!
+        **speedysource**: View my source code.\n
+        Want something else? Ask Doolan. ~~ 🐢
+    `)
         }
     },
 };
