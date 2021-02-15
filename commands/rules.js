dd379ff2a79573390d8e063d0f99cc03c071c6d3
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 17:13:07 -0800

    Revert changes to rules

--- commands/rules.js
+++ commands/rules.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'rules',
     description: 'Rules for Raiding!',
     execute(message, args) {
-        message.member.send('Here are the raid rules:', {
+        message.reply('Here are the raid rules:', {
             files: [
                 "./resources/images/rules.png"
             ]
