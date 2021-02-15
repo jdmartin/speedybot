345533c86cf49c0cef2dd10c30dc87908321554c
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-14 16:59:42 -0800

    Tweak speedy.js message

--- commands/speedy.js
+++ commands/speedy.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'speedy',
     description: 'Speedy!',
     execute(message, args) {
-        message.member.send(`Hello!  I can help by telling you stuff.  Here are some things I can do:\n
+        message.member.send(`Hello!  I can help by telling you stuff.  Here are some things I can do (commands work here or in chat):\n
             __Remember: All commands start with a **!**__\n
             **adventure**: Want a real Adventure?
             **logs**: Want the link to the logs?
