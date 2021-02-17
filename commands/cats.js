d8d89ea8117ae806ca8d2c5fcee12d79e6e8bcbd
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-16 21:52:35 -0800

    Refator of cats

--- commands/cats.js
+++ commands/cats.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'cats',
     description: 'See a random cat!',
     execute(message, args) {
-        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+        const { file } = fetch('https://aws.random.cat/meow').then(response => response.json());
 
         message.member.send(file);
     },
