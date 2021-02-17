0baaae73e884691ad2bbcb847b6c9854a9c1b470
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-16 22:53:47 -0800

    Updated speedy menu

--- commands/speedy.js
+++ commands/speedy.js
@@ -10,7 +10,8 @@ module.exports = {
             **logs**: Want the link to the logs?
             **miniwheat**: Why not flip a mini-wheat?
             **rules**: Read the Raid Rules!
-            **speedysource**: View my source code.\n
+            **speedysource**: View my source code.
+            **strats**: Did you read the strat?\n
             Want something else? Ask Doolan. ~~ 🐢
         `)
     },
