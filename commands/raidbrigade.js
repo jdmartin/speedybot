8996efc3584b4ffecad6f87ca34dc466a5a76d0c
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-17 13:44:11 -0800

    Path error in raidbrigade

--- commands/raidbrigade.js
+++ commands/raidbrigade.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'raidbrigade',
 	description: 'What\'s the code?',
 	execute(message, args) {
-        const config = require("./config.json");
+        const config = require("../config.json");
 		message.reply(`Here's the code for the in-game Raid Brigade channel: **${config.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance`);
 	},
 };
