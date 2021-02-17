e0cd53dc0c200799e1281d96a7ef6aabe52b1393
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-17 13:52:04 -0800

    Slight layout tweak to raidbrigade

--- commands/raidbrigade.js
+++ commands/raidbrigade.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'What\'s the code?',
 	execute(message, args) {
         const config = require("../config.json");
-		message.reply(`Here's the code for the in-game Raid Brigade channel: **${config.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance`);
+		message.reply(`Here's the code for the in-game Raid Brigade channel: **${config.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance \n`);
 	},
 };
