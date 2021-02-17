9ed1ddcc881e71f608b2ee6861202e7d3b6d2e7a
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-16 23:02:40 -0800

    Better channel handling for speedy?

--- commands/speedy.js
+++ commands/speedy.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'speedy',
     description: 'Speedy!',
     execute(message, args) {
-        message.member.send(`Hello!  Here are some things I can do (commands work here or in chat):\n
+        if (message.channel.type === 'dm') {
+            message.reply(`Hello!  Here are some things I can do (commands work here or in chat):\n
             __Remember: All commands start with a **!**__\n
             **adventure**: Want a real Adventure?
             **askspeedy**: Need a *guaranteed* helpful answer? Ask away.
@@ -14,5 +15,19 @@ module.exports = {
             **strats**: Did you read the strat?\n
             Want something else? Ask Doolan. ~~ 🐢
         `)
+        } else {
+            message.member.send(`Hello!  Here are some things I can do (commands work here or in chat):\n
+                __Remember: All commands start with a **!**__\n
+                **adventure**: Want a real Adventure?
+                **askspeedy**: Need a *guaranteed* helpful answer? Ask away.
+                **cats**: Need a cat right now? Let's get one from the Internet...
+                **logs**: Want the link to the logs?
+                **miniwheat**: Why not flip a mini-wheat?
+                **rules**: Read the Raid Rules!
+                **speedysource**: View my source code.
+                **strats**: Did you read the strat?\n
+                Want something else? Ask Doolan. ~~ 🐢
+            `)
+        }
     },
 };
