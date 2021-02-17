376287293c8192aeb61a5d499abf4138475b44c9
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-17 03:00:17 -0800

    Better channel handling for strats

--- commands/strats.js
+++ commands/strats.js
@@ -12,9 +12,13 @@ module.exports = {
             .addFields({name: "Bosses", value: "[Shriekwing](https://discord.com/channels/308622057707536385/308626596623810562/793739928093065236)\n[Huntsman](https://discord.com/channels/308622057707536385/308626596623810562/793740029323247646)\n[Devourer](https://discord.com/channels/308622057707536385/308626596623810562/793740126454284328)\n[Darkvein](https://discord.com/channels/308622057707536385/308626596623810562/796565645612285972)\n[Xy'Mox](https://discord.com/channels/308622057707536385/308626596623810562/795513357817479219)", inline: true})
             .addFields({name: "Even More Bosses", value: "[Sun King](https://discord.com/channels/308622057707536385/308626596623810562/796099898201341952)\n[Council](https://discord.com/channels/308622057707536385/308626596623810562/796831060709081160)\n[Sludgefist](https://discord.com/channels/308622057707536385/308626596623810562/797714826297344070)\n[Stone Legion](https://discord.com/channels/308622057707536385/308626596623810562/797999175044038656)", inline: true})
             .addFields({name: "Denathrius", value: "[Phase 1](https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075)\n[Phase 2](https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058)\n[Phase 3](https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646)", inline: true})
-
-        message.reply(`It's dangerous to go alone!  Take these:\n`);
-        message.channel.send(embed);
         
+        if (message.channel.type === 'dm') {
+            message.reply(`It's dangerous to go alone!  Take these:\n`);
+            message.channel.send(embed);
+        } else {
+            message.member.send(`It's dangerous to go alone!  Take these:\n`);
+            message.member.send(embed);
+        }
     },
 };
