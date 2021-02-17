c97aff498f773ac6267166448a56b124bf548727
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-17 02:50:15 -0800

    Implemented new strats, saved old as simplestrats

---
+++ commands/simplestrats.js
@@ -0,0 +1,45 @@
+module.exports = {
+    name: 'strats',
+    description: 'Did you read the strat?',
+    execute(message, args) {
+        if (message.channel.type === 'dm') {
+            message.reply(`Hello!  Here are links to Evie's raid strats:\n
+                __Castle Nathria__\n
+                **Shriekwing**: https://discord.com/channels/308622057707536385/308626596623810562/793739928093065236
+                **Huntsman**: https://discord.com/channels/308622057707536385/308626596623810562/793740029323247646
+                **Devourer**: https://discord.com/channels/308622057707536385/308626596623810562/793740126454284328
+                **Xy'Mox**: https://discord.com/channels/308622057707536385/308626596623810562/795513357817479219
+                **Darkvein**: https://discord.com/channels/308622057707536385/308626596623810562/796565645612285972
+                **Sun King**: https://discord.com/channels/308622057707536385/308626596623810562/796099898201341952
+                **Council**: https://discord.com/channels/308622057707536385/308626596623810562/796831060709081160
+                **Sludgefist**: https://discord.com/channels/308622057707536385/308626596623810562/797714826297344070
+                **Stone Legion**: https://discord.com/channels/308622057707536385/308626596623810562/797999175044038656
+                __Denathrius__
+                **Phase 1**: https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075
+                **Phase 2**: https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058
+                **Phase 3**: https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646 
+                \n
+                🐢
+            `)
+        } else {
+            message.member.send(`Hello!  Here are links to Evie's raid strats:\n
+                __Castle Nathria__\n
+                **Shriekwing**: https://discord.com/channels/308622057707536385/308626596623810562/793739928093065236
+                **Huntsman**: https://discord.com/channels/308622057707536385/308626596623810562/793740029323247646
+                **Devourer**: https://discord.com/channels/308622057707536385/308626596623810562/793740126454284328
+                **Xy'Mox**: https://discord.com/channels/308622057707536385/308626596623810562/795513357817479219
+                **Darkvein**: https://discord.com/channels/308622057707536385/308626596623810562/796565645612285972
+                **Sun King**: https://discord.com/channels/308622057707536385/308626596623810562/796099898201341952
+                **Council**: https://discord.com/channels/308622057707536385/308626596623810562/796831060709081160
+                **Sludgefist**: https://discord.com/channels/308622057707536385/308626596623810562/797714826297344070
+                **Stone Legion**: https://discord.com/channels/308622057707536385/308626596623810562/797999175044038656
+                __Denathrius__
+                **Phase 1**: https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075
+                **Phase 2**: https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058
+                **Phase 3**: https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646 
+                \n
+                🐢
+            `)
+        }
+    },
+};
--- commands/strats.js
+++ commands/strats.js
@@ -1,45 +1,20 @@
 module.exports = {
-    name: 'strats',
+    name: 'xyzzy',
     description: 'Did you read the strat?',
     execute(message, args) {
-        if (message.channel.type === 'dm') {
-            message.reply(`Hello!  Here are links to Evie's raid strats:\n
-                __Castle Nathria__\n
-                **Shriekwing**: https://discord.com/channels/308622057707536385/308626596623810562/793739928093065236
-                **Huntsman**: https://discord.com/channels/308622057707536385/308626596623810562/793740029323247646
-                **Devourer**: https://discord.com/channels/308622057707536385/308626596623810562/793740126454284328
-                **Xy'Mox**: https://discord.com/channels/308622057707536385/308626596623810562/795513357817479219
-                **Darkvein**: https://discord.com/channels/308622057707536385/308626596623810562/796565645612285972
-                **Sun King**: https://discord.com/channels/308622057707536385/308626596623810562/796099898201341952
-                **Council**: https://discord.com/channels/308622057707536385/308626596623810562/796831060709081160
-                **Sludgefist**: https://discord.com/channels/308622057707536385/308626596623810562/797714826297344070
-                **Stone Legion**: https://discord.com/channels/308622057707536385/308626596623810562/797999175044038656
-                __Denathrius__
-                **Phase 1**: https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075
-                **Phase 2**: https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058
-                **Phase 3**: https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646 
-                \n
-                🐢
-            `)
-        } else {
-            message.member.send(`Hello!  Here are links to Evie's raid strats:\n
-                __Castle Nathria__\n
-                **Shriekwing**: https://discord.com/channels/308622057707536385/308626596623810562/793739928093065236
-                **Huntsman**: https://discord.com/channels/308622057707536385/308626596623810562/793740029323247646
-                **Devourer**: https://discord.com/channels/308622057707536385/308626596623810562/793740126454284328
-                **Xy'Mox**: https://discord.com/channels/308622057707536385/308626596623810562/795513357817479219
-                **Darkvein**: https://discord.com/channels/308622057707536385/308626596623810562/796565645612285972
-                **Sun King**: https://discord.com/channels/308622057707536385/308626596623810562/796099898201341952
-                **Council**: https://discord.com/channels/308622057707536385/308626596623810562/796831060709081160
-                **Sludgefist**: https://discord.com/channels/308622057707536385/308626596623810562/797714826297344070
-                **Stone Legion**: https://discord.com/channels/308622057707536385/308626596623810562/797999175044038656
-                __Denathrius__
-                **Phase 1**: https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075
-                **Phase 2**: https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058
-                **Phase 3**: https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646 
-                \n
-                🐢
-            `)
-        }
+        const Discord = require("discord.js");
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Evie's Strats!")
+            .setColor(0xFFFFFF)
+            .setAuthor("🧛‍♀️")
+            .setDescription("__Castle Nathria__\n\n")
+            .setFooter("SpeedyBot is not responsible for any fire-standing, mutilation, or permanent loss of gold or seaweed...")
+            .addFields({name: "Bosses", value: "[Shriekwing](https://discord.com/channels/308622057707536385/308626596623810562/793739928093065236)\n[Huntsman](https://discord.com/channels/308622057707536385/308626596623810562/793740029323247646)\n[Devourer](https://discord.com/channels/308622057707536385/308626596623810562/793740126454284328)\n[Darkvein](https://discord.com/channels/308622057707536385/308626596623810562/796565645612285972)\n[Xy'Mox](https://discord.com/channels/308622057707536385/308626596623810562/795513357817479219)", inline: true})
+            .addFields({name: "Even More Bosses", value: "[Sun King](https://discord.com/channels/308622057707536385/308626596623810562/796099898201341952)\n[Council](https://discord.com/channels/308622057707536385/308626596623810562/796831060709081160)\n[Sludgefist](https://discord.com/channels/308622057707536385/308626596623810562/797714826297344070)\n[Stone Legion](https://discord.com/channels/308622057707536385/308626596623810562/797999175044038656)", inline: true})
+            .addFields({name: "Denathrius", value: "[Phase 1](https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075)\n[Phase 2](https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058)\n[Phase 3](https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646)", inline: true})
+
+        message.reply(`It's dangerous to go alone!  Take these:\n`);
+        message.channel.send(embed);
+        
     },
 };
