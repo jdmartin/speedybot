21ddcbafd7a9d634ded920decfac20e38286e4cd
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-17 13:42:39 -0800

    Add raidbrigade

---
+++ commands/raidbrigade.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'raidbrigade',
+	description: 'What\'s the code?',
+	execute(message, args) {
+        const config = require("./config.json");
+		message.reply(`Here's the code for the in-game Raid Brigade channel: **${config.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance`);
+	},
+};
