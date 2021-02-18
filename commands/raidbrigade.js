3e138b6ca03880c8e682732de7c7bf85bfd9b300
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-18 14:01:51 -0800

    Slight layout tweak to raidbrigade

--- commands/raidbrigade.js
+++ commands/raidbrigade.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'What\'s the code?',
 	execute(message, args) {
         require("dotenv").config();
-		message.reply(`Here's the code for the in-game Raid Brigade channel: **${process.env.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance \n`);
+		message.reply(`Here's the code for the in-game Raid Brigade channel: **${process.env.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance`);
 	},
 };
