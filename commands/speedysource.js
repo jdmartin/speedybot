dcb3486bf0ea86192d7ebc0db6d6316b6e9f02cc
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-18 15:02:23 -0800

    Updated speedysource formatting

--- commands/speedysource.js
+++ commands/speedysource.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'speedysource',
 	description: 'See my source code!',
 	execute(message, args) {
-		const response = (`My source code is here: https://github.com/jdmartin/speedybot\nThis is the latest release: https://github.com/jdmartin/speedybot/releases/latest`)
+		const response = (`My source code is here: https://github.com/jdmartin/speedybot\nThis is the latest release: https://github.com/jdmartin/speedybot/releases/latest/`)
 		message.reply(response);
 	},
 };
