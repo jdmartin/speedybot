0cf390c6918a251faa4d36c4d03366e20263b7de
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 02:04:15 -0800

    Back to basics for xk

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'xkcd',
     description: 'Get a random xkcd comic!',
     execute(message, args) {
-        function getRandomArbitrary(min, max) {
-            return Math.random() * (max - min) + min;
-        }
+//        function getRandomArbitrary(min, max) {
+//            return Math.random() * (max - min) + min;
+//        }
 
         const fetch = require('node-fetch');
         
@@ -13,12 +13,11 @@ module.exports = {
         } = fetch('https://xkcd.com/info.0.json').then(response => response.json()).then();
         
         var latestComic = num; 
-
+        message.reply(latestComic);
 //        (async function (num) {
   //          const choice = getRandomArbitrary(1, num);
     //        const {
       //          img
         //    } = fetch(`https://xkcd.com/${choice}/info.0.json`).then(response => response.json());
-            message.reply(latestComic);
     },
 };
