27d199be60233fb728ca9cce80dd4985a809f705
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 01:23:18 -0800

    Testing second part of xk

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -2,12 +2,21 @@ module.exports = {
     name: 'xkcd',
     description: 'Get a random xkcd comic!',
     execute(message, args) {
+        function getRandomArbitrary(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+
         const fetch = require('node-fetch');
         (async function () {
             const {
                 num
             } = await fetch('https://xkcd.com/info.0.json').then(response => response.json());
-            message.reply(num);
+            const choice = getRandomArbitrary(1, num);
+            const {
+                img
+            } = await fetch('https://xkcd.com/${choice}/info.0.json').then(response => response.json());
+            message.reply(img);
         })();
     },
 };
