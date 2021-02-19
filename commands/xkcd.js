5d7d129c604792ea531e532a9a17d063a38c51fb
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 02:49:12 -0800

    Uh, YOLO, I guess?

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -3,20 +3,17 @@ module.exports = {
     description: 'Get a random xkcd comic!',
     execute(message, args) {
         const fetch = require('node-fetch');
-
-        (async function () {
-            const {
-            num
-        } = fetch('https://xkcd.com/info.0.json').then(response => response.json());
-            const choice = Math.round(Math.random() * (`${num}` - 1) + 1);
-            message.reply(choice);
-    })();
-        //        
-        //       (async function () {
-        //         const {
-        //           img
-        //     } = await fetch(`https://xkcd.com/${choice}/info.0.json`).then(response => response.json());
-        //   message.reply(img);
-        //})();
+        
+        const latest = await fetch('https://xkcd.com/info.0.json').then(response => response.json());
+        const num = await latest.json();
+        
+        const choice = Math.round(Math.random() * (`${num}` - 1) + 1);
+      
+        (async function getImg () {
+                 const {
+                   img
+             } = await fetch(`https://xkcd.com/${choice}/info.0.json`).then(response => response.json());
+             message.reply(img);
+        })();
     },
 };
