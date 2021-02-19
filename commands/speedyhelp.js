a2a8bdb147c2d07a1dd8a423981beba013627b31
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 11:54:59 -0800

    Fussing with code block in speedyhelp

--- commands/speedyhelp.js
+++ commands/speedyhelp.js
@@ -6,11 +6,11 @@ module.exports = {
         __Setup__\n
         Some of the commands (!strats, !xkcd) use the embeds feature to present things in a nicer way.
         If you can't see these, and you'd like to, do this:
-        ```
+        \`\`\`
         1. Open your settings via the gear at the bottom.
         2. Go to **Text & Images**
         3. Enable 'Link Preview.'
-        ```
+        \`\`\`
         If you don't want to do that, it's ok.  I've implemented a simple view of strats (!simplestrats), and 
         will work on a solution for xkcd.
 
