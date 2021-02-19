1476bfcc53a02e62e1ee106056e25aa91b78f46c
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 12:09:28 -0800

    Refinements to xkcd

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -11,23 +11,10 @@ module.exports = {
 
             const choice = Math.round(Math.random() * (num - 1) + 1);
             
-            const {
-                img
-            } = await fetch(`https://xkcd.com/${choice}/info.0.json`).then(response => response.json());
-            
-            const Discord = require("discord.js");
-            const embed = new Discord.MessageEmbed()
-            .setTitle("xkcd")
-            .addFields({name: "Link to Original", value: `https://xkcd.com/${choice}/`})
-            .setImage(img)
-            
-            
             if (message.channel.type === 'dm') {
-                message.reply(`Here you go:\n\n(If you don't see anything, try !speedyhelp.)`);
-                message.channel.send(embed);
+                message.reply(`Here's a random comic from xkcd.com: https://xkcd.com/${choice}/\n`);
             } else {
-                message.member.send(`Here you go:\n\n(If you don't see anything, try !speedyhelp.)`);
-                message.member.send(embed);
+                message.member.send(`Here's a random comic from xkcd.com: https://xkcd.com/${choice}/\n`);
             }
         })();
     },
