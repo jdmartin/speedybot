fb3f77e5bd6027b26bb87f7336f6884676cd564a
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 11:43:38 -0800

    One day, no typos.. but not today.

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -18,7 +18,7 @@ module.exports = {
             const Discord = require("discord.js");
             const embed = new Discord.MessageEmbed()
             .setTitle("xkcd")
-            .AddFields(`{name: "Link to Original", value: "https://xkcd.com/${choice}/"}`)
+            .addFields(`{name: "Link to Original", value: "https://xkcd.com/${choice}/"}`)
             .setImage(img)
             
             
