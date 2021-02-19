3bf7bbccadcacf579ef1fe8da5dd60f10dbd0d4a
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 01:26:30 -0800

    Further testing second part of xk

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -6,13 +6,15 @@ module.exports = {
             return Math.random() * (max - min) + min;
         }
 
-
         const fetch = require('node-fetch');
         (async function () {
             const {
                 num
             } = await fetch('https://xkcd.com/info.0.json').then(response => response.json());
-            const choice = getRandomArbitrary(1, num);
+        })();
+        
+        const choice = getRandomArbitrary(1, num);
+        (async function () {
             const {
                 img
             } = await fetch('https://xkcd.com/${choice}/info.0.json').then(response => response.json());
