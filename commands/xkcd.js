b6233d6aa08cb37712fa85bd1110c45089d0a463
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 03:20:44 -0800

    So dumb it might work...

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -3,6 +3,13 @@ module.exports = {
     description: 'Get a random xkcd comic!',
     execute(message, args) {
         const fetch = require('node-fetch');
+        
+        (async function getImg (choice) {
+            const {
+            img
+        } = await fetch(`https://xkcd.com/${choice}/info.0.json`).then(response => response.json());
+        message.reply(img);
+    })();
 
         (async function getANum () {
             const {
@@ -10,13 +17,6 @@ module.exports = {
         } = await fetch('https://xkcd.com/info.0.json').then(response => response.json());
             const choice = Math.round(Math.random() * (num - 1) + 1);
             getImg(choice);
-    })();
-                
-        (async function getImg (choice) {
-            const {
-            img
-        } = await fetch(`https://xkcd.com/${choice}/info.0.json`).then(response => response.json());
-        message.reply(img);
     })();
         getANum();
     },
