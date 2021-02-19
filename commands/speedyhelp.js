f77a7562e87d3d6059ac6f71a90cdaa147e29e9c
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 11:53:52 -0800

    First pass of speedyhelp

---
+++ commands/speedyhelp.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: 'speedyhelp',
+    description: 'How can Speedy help you?',
+    execute(message, args) {
+        const strats = (`Hello!  So, a couple of things:\n
+        __Setup__\n
+        Some of the commands (!strats, !xkcd) use the embeds feature to present things in a nicer way.
+        If you can't see these, and you'd like to, do this:
+        ```
+        1. Open your settings via the gear at the bottom.
+        2. Go to **Text & Images**
+        3. Enable 'Link Preview.'
+        ```
+        If you don't want to do that, it's ok.  I've implemented a simple view of strats (!simplestrats), and 
+        will work on a solution for xkcd.
+
+        Other questions? Reach out to Doolan.
+        🐢
+    `)
+    
+        if (message.channel.type === 'dm') {
+            message.reply(strats)
+        } else {
+            message.member.send(strats)
+        }
+    },
+};
