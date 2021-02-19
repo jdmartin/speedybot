49adf6e32b1c750be13422dc7d23e535f435feb5
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-19 02:14:06 -0800

    Slowly fixing mess

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -8,14 +8,15 @@ module.exports = {
 
         const fetch = require('node-fetch');
         const num = (async function () {
-            const {
-                num
-            } = await fetch('https://xkcd.com/info.0.json').then(response => response.json());
-            message.reply(num);
+        const {
+            num
+        } = await fetch('https://xkcd.com/info.0.json').then(response => response.json());
+        const choice = getRandomArbitrary(1, num);
+        message.reply(choice);
             return num
         })();
         
-//        const choice = getRandomArbitrary(1, num);
+//        
  //       (async function () {
    //         const {
      //           img
