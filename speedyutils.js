a6531f63ead7cef7656cc93f64e2e4f383dc3507
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-20 23:20:10 -0800

    Refactored index.js, created some modules

---
+++ speedyutils.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+class CreateCommandSet {
+    generateSet() {
+        for (const file of commandFiles) {
+            const command = require(`./commands/${file}`);
+            client.commands.set(command.name, command);
+        }
+    }
+}
+
+module.exports = {
+    client: client,
+    commandFiles: commandFiles,
+    CreateCommandSet
+};
