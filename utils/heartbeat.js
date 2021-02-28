ec083228a54255963be8b23a003a0912913b273b
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-27 16:59:45 -0800

    Made port a config variable

--- utils/heartbeat.js
+++ utils/heartbeat.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = ${process.env.speedyport}
+const port = process.env.speedyport;
 
 const helmet = require('helmet');
 
