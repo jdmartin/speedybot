7d1816384d4be0a68c0cdef358404ad40def6b35
jdmartin <jonsaccount+githubjd@icloud.com>
2021-03-18 16:11:42 -0700

    Updated simplestrats

--- commands/simplestrats.js
+++ commands/simplestrats.js
@@ -16,9 +16,8 @@ module.exports = {
         __Denathrius__
         **Phase 1**: https://discord.com/channels/308622057707536385/308626596623810562/799400259205464075
         **Phase 2**: https://discord.com/channels/308622057707536385/308626596623810562/799412289530626058
-        **Phase 3**: https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646 
-        \n
-        __Heroic__
+        **Phase 3**: https://discord.com/channels/308622057707536385/308626596623810562/799452523735023646
+        \n__Heroic__
         **Shriekwing**: https://discord.com/channels/308622057707536385/308626596623810562/821981070320861184
         **Huntsman**: https://discord.com/channels/308622057707536385/308626596623810562/821981177593331713
         **Destroyer**: https://discord.com/channels/308622057707536385/308626596623810562/821981301858631680
