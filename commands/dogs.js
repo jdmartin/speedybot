c1239710ebeb574b6531ddb466f8568fcedcdda4
jdmartin <jonsaccount+githubjd@icloud.com>
2021-04-12 14:01:23 -0700

    Maybe fixed name re-use with dogs?

--- commands/dogs.js
+++ commands/dogs.js
@@ -1,21 +1,21 @@
 module.exports = {
     name: 'dogs',
     description: 'See a random dog!',
-    execute(message, args) {
+    execute(ds_message, args) {
         const fetch = require('node-fetch');
         if (!args.length) {
             (async function () {
                 const {
                     message
                 } = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
-                message.reply(message);
+                ds_message.reply(message);
             })();
         } else if (args[0] === 'corgi') {
             (async function () {
                 const {
                     message
                 } = await fetch('https://dog.ceo/api/breed/corgi/images/random').then(response => response.json());
-                message.reply(message);
+                ds_message.reply(message);
             })();
         }
     },
