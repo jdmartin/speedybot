5f5942c0b51522b2628eff1f512283ef900e0665
jdmartin <jonsaccount+githubjd@icloud.com>
2021-05-11 14:16:00 -0700

    first try at next and simplenext

---
+++ commands/next.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: 'next',
+    description: 'What are we doing?',
+    execute(message, args) {
+        const Discord = require("discord.js");
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Evie's Announcements!")
+            .setColor(0xFFFFFF)
+            .setAuthor("🧛‍♀️")
+            .setDescription("Coming Up")
+            .setFooter("SpeedyBot is not responsible for any fire-standing, mutilation, or permanent loss of gold or seaweed...")
+            .addFields({
+                name: "Immediate",
+                value: "[Posted May 9](https://discord.com/channels/308622057707536385/308625441546043402/841157209764397067)\n",
+                inline: true
+            })
+            .addFields({
+                name: "Planning",
+                value: "[The Burning Reconquista](https://discord.com/channels/308622057707536385/308625441546043402/839871852045664337)\n",
+                inline: true
+            })
+            .addFields({
+                name: "Reminders",
+                value: "Don't forget to check the #annoucements channel for more stuff!",
+                inline: true
+            })
+
+        if (message.channel.type === 'dm') {
+            message.reply(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplenext or !speedyhelp.)`);
+            message.channel.send(embed);
+        } else {
+            message.member.send(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplenext or !speedyhelp.)`);
+            message.member.send(embed);
+        }
+    },
+};
---
+++ commands/simplenext.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'simplenext',
+    description: 'What are we doing?',
+    execute(message, args) {
+        const strats = (`Hello!  Here are links to Evie's raid annoucements:\n
+        __Immediate__\n
+        **Posted May 9**: https://discord.com/channels/308622057707536385/308625441546043402/841157209764397067\n
+        __Planning__
+        **The Burning Reconquista**: https://discord.com/channels/308622057707536385/308625441546043402/839871852045664337\n
+        __Reminders__
+        Don't forget to check the #annoucements channel for more stuff!
+        🐢
+    `)
+    
+        if (message.channel.type === 'dm') {
+            message.reply(strats)
+        } else {
+            message.member.send(strats)
+        }
+    },
+};
