2a9ca76fbba85ace882364ae82cbbc9eec133702
jdmartin <jonsaccount+githubjd@icloud.com>
2021-05-12 19:50:30 -0700

    Testing sheep

---
+++ commands/sheep.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'sheep',
+	description: 'CC!',
+	execute(message, args) {
+        var fs = require('fs');
+        var files = fs.readdirSync('../images/sheep/');
+		let chosenFile = files[Math.floor(Math.random() * files.length)];
+		message.reply(`${chosenFile}`);
+	},
+};
