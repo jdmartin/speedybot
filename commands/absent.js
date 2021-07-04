a68814e8a71a135b1986cab446ea7059e0638426
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 15:44:23 -0700

    Testing user insertion in absent

--- commands/absent.js
+++ commands/absent.js
@@ -2,11 +2,9 @@ module.exports = {
 	name: 'absent',
 	description: 'Declare an absence from raid',
 	execute(message, args) {
+        let user = message.author;
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DatabaseTools();
-        client.on('message', message => {
-            let user = message.author;            
-            absenceDBHelper.test(user, message);
-        });
+        absenceDBHelper.test(user, message);
   	},
 };
