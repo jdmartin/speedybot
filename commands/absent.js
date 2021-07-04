89b12015e22390f58692d5b6e5cc4306889cc28e
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 14:52:14 -0700

    Testing absent

--- commands/absent.js
+++ commands/absent.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'absent',
 	description: 'Declare an absence from raid',
 	execute(message, args) {
-        const absencedb = require("./db/absencedb.js");
+        const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DatabaseTools();
         absenceDBHelper.test();
 	},
