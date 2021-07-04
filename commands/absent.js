b2a001c5d7e01544c0edeecb79fa49d27fd0ca62
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 15:47:39 -0700

    Print debugging FTW

--- commands/absent.js
+++ commands/absent.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'absent',
 	description: 'Declare an absence from raid',
 	execute(message, args) {
-        console.log(message.author);
+        console.log(message);
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DatabaseTools();
         absenceDBHelper.test(message);
