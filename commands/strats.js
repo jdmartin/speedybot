045a7d9eaaeef21a8ccea76ddf1f9007d96dc1ad
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-03 22:52:32 -0700

    Added sanctum to strats

--- commands/strats.js
+++ commands/strats.js
@@ -3,7 +3,18 @@ module.exports = {
     description: 'Did you read the strat?',
     execute(message, args) {
         const Discord = require("discord.js");
-        const embed = new Discord.MessageEmbed()
+        const sanctum = new Discord.MessageEmbed()
+            .setTitle("Evie's Strats!")
+            .setColor(0xFFFFFF)
+            .setAuthor("🏹")
+            .setDescription("__Sanctum of Domination__")
+            .setFooter("SpeedyBot is not responsible for any fire-standing, mutilation, or permanent loss of gold or seaweed...")
+            .addFields({
+                name: "Bosses",
+                value: "[The Tarragrue](https://discord.com/channels/308622057707536385/308626596623810562/861108078976172072)\n[Eye of the Jailer](https://discord.com/channels/308622057707536385/308626596623810562/861108185917161473)\n[The Nine](https://discord.com/channels/308622057707536385/308626596623810562/861108293966495747)\n[Ner'zhul](https://discord.com/channels/308622057707536385/308626596623810562/861108398400471060)\nWant Nathria? Use `!strats nathria`",
+                inline: true
+            })
+        const nathria = new Discord.MessageEmbed()
             .setTitle("Evie's Strats!")
             .setColor(0xFFFFFF)
             .setAuthor("🧛‍♀️")
@@ -28,13 +39,22 @@ module.exports = {
                 name: "Heroic",
                 value: "[Shriekwing](https://discord.com/channels/308622057707536385/308626596623810562/821981070320861184)\n[Huntsman](https://discord.com/channels/308622057707536385/308626596623810562/821981177593331713)\n[Destroyer](https://discord.com/channels/308622057707536385/308626596623810562/821981301858631680)\n[Darkvein](https://discord.com/channels/308622057707536385/308626596623810562/821981414957514753)\n[Xy'Mox](https://discord.com/channels/308622057707536385/308626596623810562/821981523372539954)"
             })
-
-        if (message.channel.type === 'dm') {
-            message.reply(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplestrats or !speedyhelp.)`);
-            message.channel.send(embed);
-        } else {
-            message.member.send(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplestrats or !speedyhelp.)`);
-            message.member.send(embed);
+        if (!args.length) {
+            if (message.channel.type === 'dm') {
+                message.reply(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplestrats or !speedyhelp.)`);
+                message.channel.send(sanctum);
+            } else {
+                message.member.send(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplestrats or !speedyhelp.)`);
+                message.member.send(sanctum);
+            }
+        } else if (args[0].toLowerCase() === 'nathria') {
+            if (message.channel.type === 'dm') {
+                message.reply(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplestrats or !speedyhelp.)`);
+                message.channel.send(nathria);
+            } else {
+                message.member.send(`It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try !simplestrats or !speedyhelp.)`);
+                message.member.send(nathria);
+            }
         }
     },
 };
