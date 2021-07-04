01e2ad71cc411a781bf4eab37dcc87af4a399bf0
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 15:09:12 -0700

    First test of absences

--- commands/absences.js
+++ commands/absences.js
@@ -4,6 +4,12 @@ module.exports = {
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DatabaseTools();
-        absenceDBHelper.show();
+        const absences = absenceDBHelper.show();
+
+        if (message.channel.type === 'dm') {
+            message.reply(absences)
+        } else {
+            message.member.send(absences)
+        }
 	},
 };
