6b39879b1a00963f8a4503782e4a2bfd7acd227e
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 16:40:17 -0700

    Maybe fix string error

--- db/absencedb.js
+++ db/absencedb.js
@@ -18,7 +18,7 @@ let absencedb = new sqlite3.Database('./db/absence.db', (err) => {
 
 class DatabaseTools {
     test(message) {
-        absencedb.run(`INSERT INTO absences(name, start, end, comment) VALUES (${message.author.username}, "0000-01-01", "0000-01-01", "testing")`);
+        absencedb.run(`INSERT INTO absences(name, start, end, comment) VALUES ("${message.author.username}", "0000-01-01", "0000-01-01", "testing")`);
     }
 
     show(message) {
