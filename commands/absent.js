98b51d59d1501eda5086b5275ece81192d62fbb1
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 23:18:32 -0700

    Tease out attendance commands

--- commands/absent.js
+++ commands/absent.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'absent',
 	description: 'Tell us you will miss raid!',
 	execute(message, args) {
-        const absencedb = require("./db/absencedb.js");
+        const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DatabaseTools();
 		absenceDBHelper.addAbsence(message, args);
 	},
