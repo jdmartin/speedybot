676c14fcc6c6f5092cf74c3bd0a6d72284ea4d26
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-04 23:54:07 -0700

    Improved late messaging

--- db/absencedb.js
+++ db/absencedb.js
@@ -176,7 +176,7 @@ class DatabaseTools {
                     var safe_reason = SqlString.escape(m.content);
                     absencedb.run(`INSERT INTO latecomers(name, start, comment) VALUES ("${message.author.username}", "${startDate}", "${safe_reason}")`);
                     message.author.send(`Ok, I've got you down as coming late on ${startDate}. You've indicated the reason is ${safe_reason}.\n\nIf you want to cancel this, type: !ontime ${startDate}`)
-                    client.channels.cache.get(`${process.env.attendance_channel}`).send('Hello here!')
+                    client.channels.cache.get(`${process.env.attendance_channel}`).send(`${message.author} will be late on ${startDate}. They commented: ${safe_reason}`)
                     collector.stop();
                 }
             })
