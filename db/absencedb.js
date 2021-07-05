1feb96c62ac23689ec2417c566ceb9f5d38c0a4b
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-05 00:51:50 -0700

    Try as we might: JS is not Python

--- db/absencedb.js
+++ db/absencedb.js
@@ -182,7 +182,7 @@ class DatabaseTools {
                     var safe_reason = SqlString.escape(m.content);
                     absencedb.run(`INSERT INTO latecomers(name, start, comment) VALUES ("${message.author.username}", "${startDate}", "${safe_reason}")`);
 
-                    speedy_response = `Ok, I've got you down as coming late on ${startDate}. You've indicated the reason is ${safe_reason}.\n\nIf you want to cancel this, type: !ontime ${startDate}`
+                    let speedy_response = `Ok, I've got you down as coming late on ${startDate}. You've indicated the reason is ${safe_reason}.\n\nIf you want to cancel this, type: !ontime ${startDate}`
                     
                     if (message.channel.type === 'dm') {
                         message.author.send(speedy_response);
