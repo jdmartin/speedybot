e17f596d78af287f69778dd8928325f14a016a4b
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-05 12:56:01 -0700

    Testing simpler comment system

--- db/absencedb.js
+++ db/absencedb.js
@@ -167,8 +167,8 @@ class DatabaseTools {
         }
 
         //Process a comment, if supplied.
-        let process_args = arguments;
-        console.log(process_args.shift());
+        let process_args = arguments.shift();
+        console.log(process_args);
 
         if (args[1]) {
             var safe_reason = SqlString.escape(args[1]);
