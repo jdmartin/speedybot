bd25318bee0ff0a18783741692a90ba4fb1aea1c
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-05 03:42:34 -0700

    Testing tweak to collection

--- db/absencedb.js
+++ db/absencedb.js
@@ -168,9 +168,7 @@ class DatabaseTools {
                     time: 10000
                 });
                 collector.on('end', (m, reason) => {
-                    if (reason === 'time') {
-                        message.author.send('Ran out of time...');
-                    } else if (m.content) {
+                    if (m.content) {
                         var safe_reason = SqlString.escape(m.content);
                         absencedb.run(`INSERT INTO latecomers(name, start, comment) VALUES ("${message.author.username}", "${startDate}", "${safe_reason}")`);
                         message.author.send(`Ok, I've got you down as coming late on ${startDate}. You've indicated the reason is ${safe_reason}.\n\nIf you want to cancel this, type: !ontime ${startDate}`)
