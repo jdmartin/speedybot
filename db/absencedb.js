cf8c5ca2db120d22957f4da7bf2855746abd0ba4
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-06 15:17:42 -0700

    Fix logic problem with absent end dates

--- db/absencedb.js
+++ db/absencedb.js
@@ -165,10 +165,12 @@ class DataEntryTools {
             } else {
                 var endYear = startYear;
             }
-            var rebuilt_end = args[2] + ' ' + args[3] + ' ' + endYear;
-            var endDate = tools.validateDates(message, undefined, rebuilt_end);
-            //Process Comments
-            var comment = args.slice(4).join(' ');
+            if (tools.checkIsDate(args[2], args[3], endYear)) {
+                var rebuilt_end = args[2] + ' ' + args[3] + ' ' + endYear;
+                var endDate = tools.validateDates(message, undefined, rebuilt_end);
+                //Process Comments
+                var comment = args.slice(4).join(' ');
+            }
         }
         if (endDate == undefined) {
             endDate = startDate;
