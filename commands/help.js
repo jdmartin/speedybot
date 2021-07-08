612c34db4cbff4bac49b2c7f22f4dfe5ed405415
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 23:41:40 -0700

    Modified help

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
         if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command. ~~🐢`);
 
 			return message.author.send(data, { split: true })
 				.then(() => {
