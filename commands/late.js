1a7400475c9fb1ea256383745b986c04e7ef3665
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 23:16:33 -0700

    Updated usage and notes for late

--- commands/late.js
+++ commands/late.js
@@ -1,6 +1,8 @@
 module.exports = {
 	name: 'late',
 	description: 'Let us know you will be late to raid!',
+	usage: '[Month Day Comment]',
+	notes: 'This command marks you late on a given day and allows you to supply a comment. It will then post this to the attendance channel for you.\nYou can write the whole month, or use the first three letters. Single dates can be with or without the zero. (6 or 06 is fine.)\n**Example:** `!late July 4 Feeding Clara` (Late on July 4 -- marks you on-time for just that date and supplies the reason "Feeding Clara")',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataEntryTools();
