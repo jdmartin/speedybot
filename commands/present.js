1045915a093d439a8f2b2da04588563d898d52c9
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 22:58:20 -0700

    Updated usage and notes for present

--- commands/present.js
+++ commands/present.js
@@ -1,6 +1,8 @@
 module.exports = {
 	name: 'present',
 	description: 'Tell us you will not miss raid!',
+	usage: '[Month Day]',
+	notes: 'Examples:\n!present July 4 (Present on July 4 -- removes an absence for just that date)\n!present Jul 4 Jul 21 (Present from July 4 until July 21 -- removes any absence that matches this start and end date.',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataEntryTools();
