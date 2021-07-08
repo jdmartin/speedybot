6f17f50ddf4fb1e684a20f7dcca6c88f3550c341
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 23:37:38 -0700

    Modified help

--- commands/help.js
+++ commands/help.js
@@ -11,14 +11,21 @@ module.exports = {
 		const { commands } = message.client;
 
         if (!args.length) {
-            const response = (`To find out how to use a command, type \`!help [command]\`. ~~ 🐢
-        `)
-            if (message.channel.type === 'dm') {
-                message.reply(response)
-            } else {
-                message.member.send(response)
-            }
-        }
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+
+			return message.author.send(data, { split: true })
+				.then(() => {
+					if (message.channel.type === 'dm') return;
+					message.reply('I\'ve sent you a DM with all my commands!');
+				})
+				.catch(error => {
+					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+					message.reply('it seems like I can\'t DM you!');
+				});
+		}
+
         if (args.length) {
             const name = args[0].toLowerCase();
             const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
