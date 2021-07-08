490b4d2f9381648d1fb5e29c7f171684e1feb774
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 23:00:42 -0700

    Updated usage and notes for present

--- commands/present.js
+++ commands/present.js
@@ -1,8 +1,8 @@
 module.exports = {
 	name: 'present',
 	description: 'Tell us you will not miss raid!',
-	usage: '[Month Day]',
-	notes: 'Examples:\n!present July 4 (Present on July 4 -- removes an absence for just that date)\n!present Jul 4 Jul 21 (Present from July 4 until July 21 -- removes any absence that matches this start and end date.',
+	usage: '[Month Day Month Day]',
+	notes: 'You can write the whole month, or use the first three letters. Single dates can be with or without the zero. (6 or 06 is fine.)\nExamples: !present July 4 (Present on July 4 -- removes an absence for just that date)\n!present Jul 4 Jul 21 (Present from July 4 until July 21 -- removes any absence that matches this start and end date.',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataEntryTools();
