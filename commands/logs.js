b76d27c93380e4488dcd0b6d62608627bb71e3ff
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 22:37:52 -0700

    Add alias for logs

--- commands/logs.js
+++ commands/logs.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'logs',
 	description: 'Get the Logs!',
+	aliases: ['log'],
 	execute(message, args) {
 		message.reply(`Our logs are here: https://www.warcraftlogs.com/guild/reports-list/41907/`);
 	},
