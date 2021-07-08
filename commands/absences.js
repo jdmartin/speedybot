3c0b3f6e7bb7bed62cc340ee2fe3272a8188bb30
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 23:24:42 -0700

    Add notes for absences

--- commands/absences.js
+++ commands/absences.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'absences',
 	description: 'Show absences',
+	notes: 'Shows you known absences, and declared lateness, with their comments in a concise format.',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataDisplayTools();
