6ee5317b3053fe247c8f62132465ede7add5a2d5
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-07 23:09:59 -0700

    Updated usage and notes for present and ontime

--- commands/ontime.js
+++ commands/ontime.js
@@ -1,6 +1,8 @@
 module.exports = {
 	name: 'ontime',
 	description: 'Tell us you will not be late to raid!',
+	usage: '[Month Day]',
+	notes: 'This command cancels out an entry made with !late.\nYou can write the whole month, or use the first three letters. Single dates can be with or without the zero. (6 or 06 is fine.)',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataEntryTools();
--- commands/present.js
+++ commands/present.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'present',
 	description: 'Tell us you will not miss raid!',
 	usage: '[Month Day Month Day]',
-	notes: 'You can write the whole month, or use the first three letters. Single dates can be with or without the zero. (6 or 06 is fine.)\nSingle Date Example: `!present July 4` (Present on July 4 -- removes an absence for just that date)\nDate Range Example: `!present Jul 4 Jul 21` (Present from July 4 until July 21 -- removes any absence that matches this start and end date.)',
+	notes: 'This command cancels out an entry made with !late.\nYou can write the whole month, or use the first three letters. Single dates can be with or without the zero. (6 or 06 is fine.)\nSingle Date Example: `!present July 4` (Present on July 4 -- removes an absence for just that date)\nDate Range Example: `!present Jul 4 Jul 21` (Present from July 4 until July 21 -- removes any absence that matches this start and end date.)',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataEntryTools();
