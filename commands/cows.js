8da8109659df24dc6420e3dd71ab601625b4097d
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-08 19:57:42 -0700

    Added cows

--- commands/cows.js
+++ commands/cows.js
@@ -5,6 +5,6 @@ module.exports = {
         var fs = require('fs');
         var files = fs.readdirSync('resources/images/cows/');
 		let chosenFile = files[Math.floor(Math.random() * files.length)];
-		message.reply("+1 sheep:", {files: [`resources/images/cows/${chosenFile}`]});
+		message.reply("Moooooooo:", {files: [`resources/images/cows/${chosenFile}`]});
 	},
 };
