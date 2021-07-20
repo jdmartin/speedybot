21b8cf4a9d435a1002a378bfe7f4b7fc0a020ce9
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-20 16:21:40 -0700

    New shuffle for miniwheat

--- commands/miniwheat.js
+++ commands/miniwheat.js
@@ -1,3 +1,7 @@
+const speedyutils = require('../utils/speedyutils');
+const utils = new speedyutils.SpeedyTools();
+const getRandomIntInclusive = utils.getRandomIntInclusive;
+
 module.exports = {
 	name: 'miniwheat',
 	description: 'Flip a Mini-Wheat!',
@@ -5,7 +9,6 @@ module.exports = {
 	notes: '',
 	execute(message, args) {
 		let outcomes = ["frosted", "plain"];
-		let outcomesIndex = Math.round(Math.random());
-		message.reply(`Your mini-wheat lands on the **${outcomes[outcomesIndex]}** side!`);
+		message.reply(`Your mini-wheat lands on the **${outcomes[getRandomIntInclusive(0,outcomes.length - 1)]}** side!`);
 	},
 };
