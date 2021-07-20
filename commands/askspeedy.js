28611679c759e8945237168da02f36400d8a5422
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-20 15:59:56 -0700

    New shuffle for askspeedy

--- commands/askspeedy.js
+++ commands/askspeedy.js
@@ -1,7 +1,3 @@
-const utils = require("../utils/speedyutils.js");
-const speedyutils = new utils.SpeedyTools();
-const shuffleArray = speedyutils.shuffleArray;
-
 module.exports = {
 	name: 'askspeedy',
 	description: 'Get a guaranteed quality answer to life\'s mysteries',
@@ -9,6 +5,7 @@ module.exports = {
 	notes: '',
 	execute(message, args) {
 		let divinations = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
-        message.reply(`**${shuffleArray(divinations)[0]}**`);
+		let divinationsIndex = Math.round(Math.random() * divinations.length);
+        message.reply(`**${divinations[divinationsIndex]}**`);
 	},
 };
