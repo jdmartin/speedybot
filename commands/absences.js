365ba18caa0c96f4fba560bb555fc29efa5efbeb
jdmartin <jonsaccount+githubjd@icloud.com>
2021-07-20 20:39:21 -0700

    Updated usage notes for absences

--- commands/absences.js
+++ commands/absences.js
@@ -1,8 +1,8 @@
 module.exports = {
 	name: 'absences',
 	description: 'Show absences',
-	usage: '',
-	notes: 'Shows you known absences, and declared lateness, with their comments in a concise format.',
+	usage: '[option]',
+	notes: 'Shows you known absences, and declared lateness, with their comments in a concise format.\nOptionally, you can say `!absences mine` to see just your own absences and declared lateness.',
 	execute(message, args) {
         const absencedb = require("../db/absencedb.js");
         const absenceDBHelper = new absencedb.DataDisplayTools();
