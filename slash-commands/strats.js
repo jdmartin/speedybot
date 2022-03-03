b2edb811e052bf2ea31e2b21d448534ca78dde6f
jdmartin <jonsaccount+githubjd@icloud.com>
2022-03-03 13:24:00 -0800

    Simplified /strats for ease of use

--- slash-commands/strats.js
+++ slash-commands/strats.js
@@ -6,26 +6,22 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('strats')
         .setDescription('Get all the links to strats for the current raid, or for an older raid.')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('sepulchre')
-                .setDescription('Strats for Sepulchre of the First Ones'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('sanctum')
-                .setDescription('Strats for Sanctum of Domination'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('nathria')
-                .setDescription('Strats for Castle Nathria')),
+        .addStringOption(option => 
+            option.setName('raid_name')
+            .setDescription('Name of the Raid')
+            .setRequired(true)
+            .addChoice('Sepulchre', 'sepulchre')
+            .addChoice('Sanctum', 'sanctum')
+            .addChoice('Nathria', 'nathria')),
+
     async execute(interaction) {
         const Shadowlands =  require('../resources/strats/shadowlands.js');
 
-        if (interaction.options.getSubcommand().toLowerCase() === 'sanctum') {
+        if (interaction.options.getString('raid_name') === 'sanctum') {
             interaction.reply({content: "It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try `!simplestrats` or `!speedyhelp`.)", embeds: [Shadowlands.sanctum], ephemeral: true});
-        } else if (interaction.options.getSubcommand().toLowerCase() === 'nathria') {
+        } else if (interaction.options.getString('raid_name') === 'nathria') {
             interaction.reply({content: "It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try `!simplestrats` or `!speedyhelp`.)", embeds: [Shadowlands.nathria], ephemeral: true});
-        } else if (interaction.options.getSubcommand().toLowerCase() === 'sepulchre') {
+        } else if (interaction.options.getString('raid_name') === 'sepulchre') {
             interaction.reply({content: "It's dangerous to go alone!  Take these:\n\n(If you don't see anything, try `!simplestrats` or `!speedyhelp`.)", embeds: [Shadowlands.sepulchre], ephemeral: true});
         }
     },
