e275c16dc8b2265952fbd9f3beaa8ba8cd9ec973
jdmartin <jonsaccount+githubjd@icloud.com>
2022-06-02 01:59:54 -0700

    Updated simplestrats

--- commands/simplestrats.js
+++ commands/simplestrats.js
@@ -4,6 +4,23 @@ module.exports = {
     usage: '[optional raid name]',
     notes: 'Right now, the only optional name is: nathria',
     execute(message, args) {
+        const sepulchre = (`Hello!  Here are links to Evie's raid strats:\n
+        __Sepulchre of the First Ones__\n
+        **Vigilant Guardian**: https://discord.com/channels/308622057707536385/308626596623810562/947960827326115871
+        **Skolex**: https://discord.com/channels/308622057707536385/308626596623810562/948029005741850624
+        **Xy'Mox, take 2**: https://discord.com/channels/308622057707536385/308626596623810562/955914735956668466
+        **Dausegne**: https://discord.com/channels/308622057707536385/308626596623810562/948961279786254447
+        **Pantheon**: https://discord.com/channels/308622057707536385/308626596623810562/949042645177622549
+        **Lihuvium**: https://discord.com/channels/308622057707536385/308626596623810562/952604140855525527
+        **Halondrus**: https://discord.com/channels/308622057707536385/308626596623810562/955979512582135829
+        **Anduin**: https://discord.com/channels/308622057707536385/308626596623810562/956592880695271424
+        **Rygelon**: https://discord.com/channels/308622057707536385/308626596623810562/966888525184270406
+        **Lords of Dread**: https://discord.com/channels/308622057707536385/308626596623810562/969296305472036944
+        **The Jailer**: https://discord.com/channels/308622057707536385/308626596623810562/969316069028286474
+        
+        Something else? Try !simplestrats nathria or !simplestrats sanctum
+        🐢
+    `)
         const sanctum = (`Hello!  Here are links to Evie's raid strats:\n
         __Sanctum of Domination__\n
         **The Tarragrue**: https://discord.com/channels/308622057707536385/308626596623810562/861108078976172072
@@ -52,11 +69,18 @@ module.exports = {
 
         if (!args.length) {
             if (message.channel.type === 'DM') {
+                message.channel.send(sepulchre)
+            } else {
+                message.member.send(sepulchre)
+            }
+        } else if (args[0].toLowerCase() === 'sanctum') {
+            if (message.channel.type === 'DM') {
                 message.channel.send(sanctum)
             } else {
                 message.member.send(sanctum)
             }
-        } else if (args[0].toLowerCase() === 'nathria') {
+        } 
+        else if (args[0].toLowerCase() === 'nathria') {
             if (message.channel.type === 'DM') {
                 message.channel.send(nathria)
             } else {
