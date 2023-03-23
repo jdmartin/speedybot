febae3112265f9965e308a4f487845801d491993
jdmartin <jonsaccount+githubjd@icloud.com>
2023-03-22 17:46:37 -0700

    Remove unnecessary console.log

--- utils/nameutils.js
+++ utils/nameutils.js
@@ -3,7 +3,6 @@ const utils = require("../utils/speedyutils.js");
 const client = utils.client;
 
 function asyncGetName(message) {
-    console.log(message);
     const guild = client.guilds.cache.get(process.env.guild_id);
     return Promise.resolve(guild.members.fetch(message.author.id));
 }
