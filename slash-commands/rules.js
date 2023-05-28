50005280d6e63e90c4050ca3913ba9ec3da1de77
jdmartin <jonsaccount+githubjd@icloud.com>
2023-05-27 20:47:03 -0700

    Add handy button to rules.

--- slash-commands/rules.js
+++ slash-commands/rules.js
@@ -1,17 +1,18 @@
-const {
-    SlashCommandBuilder
-} = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('rules')
-        .setDescription('Rules for Raiding!'),
+    data: new SlashCommandBuilder().setName("rules").setDescription("Rules for Raiding!"),
     async execute(interaction) {
+        const button = new ButtonBuilder()
+            .setLabel("Visit the Corkboard")
+            .setURL("https://velocitycorkboard.com")
+            .setStyle(ButtonStyle.Link);
+
+        const row = new ActionRowBuilder().addComponents(button);
         interaction.reply({
-            content: 'Here are the raid rules:',
-            files: [
-                "./resources/images/rules.png"
-            ]
+            content: "Here are the raid rules. You can also see the latest version on the Corkboard!",
+            files: ["./resources/images/rules.png"],
+            components: [row],
         });
     },
 };
