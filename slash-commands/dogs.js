9b8d708892ff33780968108419542cdc4fbf7aa3
jdmartin <jonsaccount+githubjd@icloud.com>
2023-05-27 21:03:45 -0700

    Fixed imports, tweaked next.

--- slash-commands/dogs.js
+++ slash-commands/dogs.js
@@ -1,26 +1,25 @@
-const {
-    SlashCommandBuilder
-} = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('dogs')
-        .setDescription('See a random dog!')
-        .addStringOption(option =>
-            option.setName('breed')
-            .setDescription('The dog breed')
-            .setRequired(true)
-            .addChoices(
-                {name: 'cardigan', value: 'corgi/cardigan'},
-                {name: 'corgi', value: 'corgi'},
-                {name: 'german shepherd', value: 'germanshepherd'},
-                {name: 'husky', value: 'husky'},
-                {name: 'pembroke', value: 'pembroke'},
-                {name: 'pitbull', value: 'pitbull'},
-                {name: 'poodle', value: 'poodle/standard'},
-                {name: 'pug', value: 'pug'},
-                {name: 'random', value: 'random'},
-            )
+        .setName("dogs")
+        .setDescription("See a random dog!")
+        .addStringOption((option) =>
+            option
+                .setName("breed")
+                .setDescription("The dog breed")
+                .setRequired(true)
+                .addChoices(
+                    { name: "cardigan", value: "corgi/cardigan" },
+                    { name: "corgi", value: "corgi" },
+                    { name: "german shepherd", value: "germanshepherd" },
+                    { name: "husky", value: "husky" },
+                    { name: "pembroke", value: "pembroke" },
+                    { name: "pitbull", value: "pitbull" },
+                    { name: "poodle", value: "poodle/standard" },
+                    { name: "pug", value: "pug" },
+                    { name: "random", value: "random" },
+                ),
         ),
 
     async execute(interaction) {
@@ -35,12 +34,12 @@ module.exports = {
         }
 
         async function getTheDog() {
-            if (option === 'random') {
+            if (option === "random") {
                 (async function () {
-                    https.get('https://dog.ceo/api/breeds/image/random', res => {
+                    https.get("https://dog.ceo/api/breeds/image/random", (res) => {
                         res.setEncoding("utf8");
-                        let body = '';
-                        res.on("data", data => {
+                        let body = "";
+                        res.on("data", (data) => {
                             body += data;
                         });
                         res.on("end", () => {
@@ -48,14 +47,14 @@ module.exports = {
                             let message = bodyParsed.message;
                             showTheDog(message);
                         });
-                    })
+                    });
                 })();
             } else if (option.length) {
                 (async function () {
-                    https.get(`https://dog.ceo/api/breed/${option}/images/random`, res => {
+                    https.get(`https://dog.ceo/api/breed/${option}/images/random`, (res) => {
                         res.setEncoding("utf8");
-                        let body = '';
-                        res.on("data", data => {
+                        let body = "";
+                        res.on("data", (data) => {
                             body += data;
                         });
                         res.on("end", () => {
@@ -63,7 +62,7 @@ module.exports = {
                             let message = bodyParsed.message;
                             showTheDog(message);
                         });
-                    })
+                    });
                 })();
             }
         }
