a17d26cccfa6ee626fa8341c49c7601aa4545805
jdmartin <jonsaccount+githubjd@icloud.com>
2023-05-27 21:08:11 -0700

    Fixed import, added button to raidbrigade

--- slash-commands/raidbrigade.js
+++ slash-commands/raidbrigade.js
@@ -1,13 +1,18 @@
-const {
-    SlashCommandBuilder
-} = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('raidbrigade')
-        .setDescription('What\'s the code?'),
+    data: new SlashCommandBuilder().setName("raidbrigade").setDescription("What's the code?"),
     async execute(interaction) {
+        const button = new ButtonBuilder()
+            .setLabel("Visit the Invitation Page")
+            .setURL("https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance")
+            .setStyle(ButtonStyle.Link);
+
+        const row = new ActionRowBuilder().addComponents(button);
         require("dotenv").config();
-		interaction.reply(`Here's the code for the in-game Raid Brigade channel: **${process.env.raidbrigcode}**\nNot sure how to use it? See this: https://worldofwarcraft.com/en-us/invite/K9P9EZpT39q?region=US&faction=Alliance`);
+        interaction.reply({
+            content: `Here's the code for the in-game Raid Brigade channel: **${process.env.raidbrigcode}**\n\nNot sure how to use it? Click the button! \n\n`,
+            components: [row],
+        });
     },
 };
