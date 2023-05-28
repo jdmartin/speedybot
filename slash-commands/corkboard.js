f647f188b34429df8a7b822f8d51d43bc69bbf1e
jdmartin <jonsaccount+githubjd@icloud.com>
2023-05-27 20:56:51 -0700

    Fix import and add button to corkboard.

--- slash-commands/corkboard.js
+++ slash-commands/corkboard.js
@@ -1,14 +1,16 @@
-const {
-    SlashCommandBuilder
-} = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('corkboard')
-        .setDescription('What\'s the link and code?'),
+    data: new SlashCommandBuilder().setName("corkboard").setDescription("What's the link and code?"),
     async execute(interaction) {
         require("dotenv").config();
-        let response = `The corkboard is here: https://velocitycorkboard.com, and the password is: **${process.env.CORKBOARD_PASS}**\n`;
-		interaction.reply({content: response, ephemeral: true});
-    }
+        const button = new ButtonBuilder()
+            .setLabel("Visit the Corkboard")
+            .setURL("https://velocitycorkboard.com")
+            .setStyle(ButtonStyle.Link);
+
+        const row = new ActionRowBuilder().addComponents(button);
+        let response = `The corkboard is here: https://velocitycorkboard.com, and the password is: **${process.env.CORKBOARD_PASS}**\n\n`;
+        interaction.reply({ content: response, components: [row], ephemeral: true });
+    },
 };
