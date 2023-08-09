edb7da4228d719f7c9a53b9afd817a2c10aedaac
jdmartin <jdmgit@preludes.org>
2023-08-09 15:03:57 -0700

    Exports to excel, as well

--- db/xmasdb.js
+++ db/xmasdb.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-const { EmbedBuilder } = require("discord.js");
+const { AttachmentBuilder, EmbedBuilder } = require("discord.js");
 const sqlite3 = require("better-sqlite3");
+const ExcelJS = require('exceljs');
 const currentDate = new Date();
 const currentYear = parseInt(currentDate.getFullYear());
 
@@ -45,9 +46,66 @@ class XmasTools {
 }
 
 class XmasDisplayTools {
+    async export() {
+        const elfExport = xmasdb.prepare("SELECT * FROM elves");
+        const rows = elfExport.all();
+
+        // Create a new Excel workbook and worksheet
+        const columnOrder = ['name', 'count', 'address', 'cust1', 'cust2', 'cust3', 'notes'];
+        const columnMapping = {
+            name: 'NAME',
+            count: '# OF CARDS',
+            notes: 'NOTES',
+            address: 'ADDRESS',
+            cust1: 'LIST SENT',
+            cust2: 'LIST ACK',
+            cust3: 'CARDS RECEIVED'
+        };
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet(`elves-${currentYear}.xslx`);
+
+        // Add headers to the worksheet
+        const headerRow = worksheet.addRow(columnOrder.map(column => columnMapping[column]));
+        // Set column widths (adjust values as needed)
+        headerRow.eachCell((cell, colNumber) => {
+            const columnWidths = [20, 10, 30, 10, 10, 15, 45]; // Example widths
+            const columnIndex = colNumber - 1; // ExcelJS is 1-based index, array is 0-based
+            worksheet.getColumn(colNumber).width = columnWidths[columnIndex];
+        });
+
+        // Add data rows to the worksheet
+        rows.forEach(row => {
+            // Replace newline characters with ', ' in the address field
+            row.address = row.address.replace(/\\n/g, ', ');
+
+            // Create an object representing the row with custom values
+            const newRow = {
+                ...row,
+                cust1: '', // Add custom value for cust1
+                cust2: '', // Add custom value for cust2
+                cust3: ''  // Add custom value for cust3
+            };
+
+            // Map values to columns based on columnOrder
+            const values = columnOrder.map(column => newRow[column]);
+            worksheet.addRow(values);
+        });
+
+        // Save the Excel file to a buffer
+        const buffer = await workbook.xlsx.writeBuffer();
+
+        // Create a Discord.js attachment
+        const attachmentBuilder = new AttachmentBuilder(buffer, { name: `elves-${currentYear}.xlsx` });
+
+        // Return both the attachment and buffer
+        return {
+            attachment: attachmentBuilder,
+        };
+    }
+
     show() {
         const elvesEmbed = new EmbedBuilder().setColor(0xffffff).setTitle("🧝‍♀️ Good Little Elves 🧝").setFooter({
-            text: "These good elves are known to the Infinite Speedyflight. Use this information wisely.",
+            text: "These good elves are known to the Infinite Speedyflight. (Addresses in attached spreadsheet) Use this information wisely.",
         });
         var elfSql = xmasdb.prepare("SELECT * FROM elves WHERE year = ? ORDER BY name ASC");
         var elfResults = elfSql.all(currentYear);
@@ -55,7 +113,7 @@ class XmasDisplayTools {
             elfResults.forEach((row) => {
                 elvesEmbed.addFields({
                     name: row.name,
-                    value: "Number of Cards: " + row.count + "\nNotes: " + row.notes + "\nAddress: " + row.address.replace(/\\n/g, ', '),
+                    value: "Number of Cards: " + row.count + "\nNotes: " + row.notes,
                     inline: false,
                 });
             });
