2c1b089b181084cdd9037a3bdccfbf84d18a99c1
jdmartin <jdmgit@preludes.org>
2023-08-09 11:30:34 -0700

    Tweaks to nickname getter

--- utils/nameutils.js
+++ utils/nameutils.js
@@ -2,9 +2,16 @@ require("dotenv").config();
 const utils = require("../utils/speedyutils.js");
 const client = utils.client;
 
-function asyncGetName(message) {
+async function asyncGetName(message) {
     const guild = client.guilds.cache.get(process.env.guild_id);
-    return Promise.resolve(guild.members.fetch(message.author.id));
+    const member = await guild.members.fetch(message.author.id);
+    var the_nickname = "";
+    if (member.nickname) {
+        the_nickname = member.nickname;
+    } else {
+        the_nickname = member.user.username;
+    }
+    return the_nickname
 }
 
 module.exports = {
