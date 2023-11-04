4c363a898402cc12cbd5e045c31801f91bcf87bf
jdmartin <jdmgit@preludes.org>
2023-11-04 12:22:38 -0700

    Removed cruft

--- utils/datetools.js
+++ utils/datetools.js
@@ -1,10 +1,5 @@
 const { format } = require('date-fns');
-const parse = require("date-fns/parse");
 const parseISO = require("date-fns/parseISO");
-const isTuesday = require("date-fns/isTuesday");
-const isThursday = require("date-fns/isThursday");
-const isSunday = require("date-fns/isSunday");
-const isValid = require("date-fns/isValid");
 
 class dateTools {
     determineYear(month, day) {
@@ -29,48 +24,6 @@ class dateTools {
     makeFriendlyDates(date) {
         let friendlyDate = format(parseISO(date), 'iii. MMMM dd, yyyy');
         return friendlyDate;
-    }
-
-    validateRaidDate(DM, start) {
-        if (start != undefined) {
-            //Make sure given dates are raid dates.
-            if (isValid(parse(start, "LLLL dd yyyy", new Date()))) {
-                let temp_date = parse(start, "LLLL dd yyyy", new Date());
-                let simple_date = temp_date.toISOString().split("T")[0];
-                if (
-                    isTuesday(parseISO(simple_date)) ||
-                    isThursday(parseISO(simple_date)) ||
-                    isSunday(parseISO(simple_date))
-                ) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            //Request date in proper format.
-            DM.channel.send({ content: "Sorry, I need a date in the format 'Month Day'" });
-            return;
-        }
-    }
-
-    validateSlashDates(start, end) {
-        if (start != undefined) {
-            //Make sure given dates are dates.
-            if (isValid(parse(start, "LLLL dd yyyy", new Date()))) {
-                let temp_date = parse(start, "LLLL dd yyyy", new Date());
-                let simple_date = temp_date.toISOString().split("T")[0];
-                return simple_date;
-            }
-            return;
-        }
-        if (end != undefined) {
-            if (isValid(parse(end, "LLLL dd yyyy", new Date()))) {
-                let temp_date = parse(end, "LLLL dd yyyy", new Date());
-                let simple_date = temp_date.toISOString().split("T")[0];
-                return simple_date;
-            }
-            return;
-        }
     }
 }
 
