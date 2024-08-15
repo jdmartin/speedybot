82403eedc9ff38eb566c0c1b422633c5caa23b24
jdmartin <jdmgit@preludes.org>
2024-08-14 22:03:06 -0700

    Add Info commands and update /speedy

---
+++ slash-commands/info.js
@@ -0,0 +1,126 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder().setName("info").setDescription("Answers to all your burning questions...")
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('attendance')
+                .setDescription('How do I post (or delete) an absence?'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('corkboard')
+                .setDescription('How do I check the Corkboard?'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('crafter')
+                .setDescription('How do I find a raid crafter?')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('logs')
+                .setDescription('How do I check the logs?')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('schedule')
+                .setDescription('How do I find the raid schedule?')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('signup')
+                .setDescription('How do I sign up to raid?')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('trb')
+                .setDescription('How do I join the TRB channel in game?')
+        ),
+    async execute(interaction) {
+        switch (interaction.options.getSubcommand()) {
+            case 'attendance':
+                absenceEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I post (or delete) an absence?")
+                    .addFields({
+                        name: " ",
+                        value: "\n**To update your attendance through Discord:**\n- Make sure you aren’t in a private message window. Any public channel will work, even this one!\n- Just type `/attendance` in the typing box, and Speedy will pop up a box for you to enter your information in!\n- The box and its contents do not appear as a chat post, all anyone else will see is the end result, such as “Alien will be absent tomorrow because he sent Evie Frosted claws in the mail\n\n**To update your attendance through the Corkboard:**\n- Go to the Corkboard [(link)](https://velocitycorkboard.com)\n- Fill out the form with your info (the big box on the left to add an absence, the small box on the right to cancel one)\n- Copy down the secret code at the bottom in case you need to change your answer later.",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [absenceEmbed], ephemeral: true });
+                break;
+            case 'corkboard':
+                corkEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I check the Corkboard?")
+                    .addFields({
+                        name: " ",
+                        value: "\nYou can get to the Corkboard Here [(link)](https://velocitycorkboard.com).\n\nIf you need the password:\n- Make sure you aren’t in a private message window. Any public channel will work, even this one!\n- Just type `/corkboard` in the typing box. Speedy will give you a link and the code, and no one will be able to see that you've asked!",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [corkEmbed], ephemeral: true });
+                break;
+            case 'crafter':
+                craftEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I find a raid crafter?")
+                    .addFields({
+                        name: " ",
+                        value: "\nThe master list of crafters is on a spreadsheet in the Corkboard! [(link)](https://velocitycorkboard.com).\n\nIf you need to find someone in charge of a profession, that’s where you look. If you want to _BE_ in charge of a profession, message @Evie!",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [craftEmbed], ephemeral: true });
+                break;
+            case 'logs':
+                logsEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I check the logs?")
+                    .addFields({
+                        name: " ",
+                        value: "\nDoolan keeps all the logs for the raid here [(link)](https://www.warcraftlogs.com/guild/reports-list/41907/).\n\nYou can also access them by typing `/logs` into any public chat channel, and it will post a link for all to see!",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [logsEmbed], ephemeral: true });
+                break;
+            case 'schedule':
+                schedEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I check the raid schedule?")
+                    .addFields({
+                        name: " ",
+                        value: "\nThe raid schedule is always the same during an active Tier:\n- Tuesday, Thursday, Sunday\n- Invites at 8:15\n- Raid 8:30-10:30 (Server time)\n\nDuring breaks, or when there are special circumstances, I will post the schedule each week (as soon as I know it) out in the Announcements section.\n\nThere is also a calendar on the Corkboard! [(link)](https://velocitycorkboard.com)",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [schedEmbed], ephemeral: true });
+                break;
+            case 'signup':
+                signupEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I sign up to raid?")
+                    .addFields({
+                        name: " ",
+                        value: "\nRaid signups are handled on the Corkboard through a spreadsheet. You can find it here in the Extras section [(link)](https://velocitycorkboard.com).\n\nNeed the password?  Type `/corkboard` in any public channel. You're the only one that will see the response!",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [signupEmbed], ephemeral: true });
+                break;
+            case 'trb':
+                trbEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I join the TRB community channel in game?")
+                    .addFields({
+                        name: " ",
+                        value: `\nIf you have a new alt and you want to get into the community channel:\n- Open up the Community settings thing in game and select “Join a community”.\n- When prompted for a code, paste in: ${process.env.raidbrigcode}\n\nYou can also access this by typing /raidbrigade into any public channel. Only you will see the result!`,
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [trbEmbed], ephemeral: true });
+                break;
+        }
+    },
+};
--- slash-commands/speedy.js
+++ slash-commands/speedy.js
@@ -3,31 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder().setName("speedy").setDescription("Learn more about Speedybot commands!"),
     async execute(interaction) {
-        const response = `Hello!  Here are some things I can do:\n
-            __Remember: All commands start with a **/**__\n
-            **adventure**: Want a real Adventure?
-            **askspeedy**: Need a *guaranteed* helpful answer? Ask away.
-            **astro**: See NASA's Astronomy Picture of the Day!
-            **cats**: Need a cat right now? Let's get one from the Internet...
-            **corkboard**: Where's the website? What's the password?
-            **cows**: Mooooooo!  MOO!
-            **dogs**: We've got 'em!  You can also use '/dog corgi' to get a corgi.
-            **foxes**: How about a fox instead? To the Internet!
-            **logs**: Want the link to the logs?
-            **miniwheat**: Why not flip a mini-wheat?
-            **raidbrigade**: Need the code for the in-game channel?
-            **roll**: A Dice roller. (Examples: /roll 1d20, /roll 1d20 + 1, /roll 2d20 - 3, /roll 6d6 * 2)
-            **rules**: Read the Raid Rules!
-            **sheep**: Need CC? Speedy's got you.
-            **sourcecode**: View my source code.
-            **strats**: Did you read the strat?
-            **xkcd**: Read a random comic...\n\n
-            __Attendance-related Commands__:
-            **absences**: See known absences.
-            **attendance**: Manage your raid attendance.
-
-            Want something else? Ask Doolan. ~~ 🐢
-        `;
+        const response = "Speedy Reference Manual:\n\n**__Things we can all see__:**\n- **/askspeedy**: Need a guaranteed helpful answer? Ask away.\n- **/cats**: Need a cat right now? Let's get one from the Internet...\n- **/dogs**: We've got 'em!  You can also use `/dogs corgi` to get a corgi.\n- **/foxes**: How about a fox instead? To the Internet!\n- **/logs**: Want the link to the logs?\n- **/miniwheat**: Why not flip a mini-wheat?\n- **/roll**: D&D style dice roller (Examples: `/roll 1d4 + 1`, `/roll 1d4 - 1`, `/roll 1d4 * 2`, `/roll 1d20`)\n- **/sheep**: Need CC? Speedy's got you.\n- **/xkcd**: Read a random comic...\n\n**__Things only you see__:** (Note: Only correctly formatted Attendance info gets posted to #attendance. Mistakes are hidden.)\n- **/absences**: See known absences.\n- **/astro**: See the NASA Astronomy Picture of the Day\n- **/attendance**: Manages all things attendance. :turtle:\n- **/adventure**: Want a real Adventure?\n- **/corkboard**: Where's the website? What's the password?\n- **/raidbrigade**: Need the code for the in-game channel?\n- **/rules**: Read the Raid Rules!\n- **/sourcecode**: View my source code.\n- **/speedy**: See this help message.\n- **/strats**: Did you read the strat?\n\nWant something else? Ask Doolan. ~~ :turtle:";
         interaction.reply({ content: response, ephemeral: true });
     },
 };
