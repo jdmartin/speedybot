df14605296fd092f71cb6b08929414b5c5e6672f
jdmartin <jdmgit@preludes.org>
2024-08-18 21:46:19 -0700

    Further update info

--- slash-commands/info.js
+++ slash-commands/info.js
@@ -41,6 +41,11 @@ module.exports = {
         )
         .addSubcommand(subcommand =>
             subcommand
+                .setName('strats')
+                .setDescription('How do I find the raid strats?')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
                 .setName('trb')
                 .setDescription('How do I join the TRB channel in game?')
         ),
@@ -64,7 +69,7 @@ module.exports = {
                     .setTitle("How do I post (or delete) an absence?")
                     .addFields({
                         name: " ",
-                        value: "\n**To update your attendance through Discord:**\n- Make sure you aren’t in a private message window. Any public channel will work, even this one!\n- Just type `/attendance` in the typing box, and Speedy will pop up a box for you to enter your information in!\n- The box and its contents do not appear as a chat post, all anyone else will see is the end result, such as “Alien will be absent tomorrow because he sent Evie Frosted claws in the mail\n\n**To update your attendance through the Corkboard:**\n- Go to the Corkboard [(link)](https://velocitycorkboard.com)\n- Fill out the form with your info (the big box on the left to add an absence, the small box on the right to cancel one)\n- Copy down the secret code at the bottom in case you need to change your answer later.",
+                        value: "\n**To update your attendance through Discord:**\n- Make sure you aren’t in a private message window. Any public channel will work, even this one!\n- Just type `/attendance` in the typing box, and Speedy will pop up a box for you to enter your information in!\n\b*The box and its contents do not appear as a chat post, all anyone else will see is the end result, such as “Alien will be absent tomorrow because he sent Evie Frosted claws in the mail*\n\n**To update your attendance through the Corkboard:**\n- Go to the Corkboard [(link)](https://velocitycorkboard.com) and click the attendance button on the home page.\n- You’ll see a big box on the left where you can type your info to add an absence, and a small box on the right that you can use to cancel one.\n- Copy down the secret code at the bottom in case you need to change your answer later.\n\n*The only thing that will be visible to others in Discord chat is the completed absent note, such as: \"via Corkboard: Amarii loves Ned more than you, sorry\"*",
                         inline: true
                     })
 
@@ -76,7 +81,7 @@ module.exports = {
                     .setTitle("How do I check the Corkboard?")
                     .addFields({
                         name: " ",
-                        value: "\nYou can get to the Corkboard Here [(link)](https://velocitycorkboard.com).\n\nIf you need the password:\n- Make sure you aren’t in a private message window. Any public channel will work, even this one!\n- Just type `/corkboard` in the typing box. Speedy will give you a link and the code, and no one will be able to see that you've asked!",
+                        value: "\nYou can get to the Corkboard Here [(link)](https://velocitycorkboard.com).\n\nIf you need the password:\n- Make sure you aren’t in a private message window. Any public channel will work, even this one!\n- Just type `/corkboard` in the typing box. Speedy will give you a link and the code.\n\n*None of the above will be visible to others in the Discord chat.*",
                         inline: true
                     })
 
@@ -88,7 +93,7 @@ module.exports = {
                     .setTitle("How do I find a raid crafter?")
                     .addFields({
                         name: " ",
-                        value: "\nThe master list of crafters is on a spreadsheet in the Corkboard! [(link)](https://velocitycorkboard.com).\n\nIf you need to find someone in charge of a profession, that’s where you look. If you want to _BE_ in charge of a profession, message @Evie!",
+                        value: "\n**The master list of crafters is on a spreadsheet in the Corkboard!** [(link)](https://velocitycorkboard.com).\n\n- If you need to find someone in charge of a profession, check the spreadsheet.\n- If you want to *BE* in charge of a profession, message @Evie!",
                         inline: true
                     })
 
@@ -100,7 +105,7 @@ module.exports = {
                     .setTitle("How do I check the logs?")
                     .addFields({
                         name: " ",
-                        value: "\nDoolan keeps all the logs for the raid here [(link)](https://www.warcraftlogs.com/guild/reports-list/41907/).\n\nYou can also access them by typing `/logs` into any public chat channel, and it will post a link for all to see!",
+                        value: "\n- Doolan keeps all the logs for the raid here [(link)](https://www.warcraftlogs.com/guild/reports-list/41907/).\n- You can also access them by typing `/logs` into any public chat channel, and it will post a link for all to see!\n\n*Typing /logs in Discord will produce a visible link, anyone can see that you have asked for them.*",
                         inline: true
                     })
 
@@ -124,7 +129,7 @@ module.exports = {
                     .setTitle("How do I check the raid schedule?")
                     .addFields({
                         name: " ",
-                        value: "\nThe raid schedule is always the same during an active Tier:\n- Tuesday, Thursday, Sunday\n- Invites at 8:15\n- Raid 8:30-10:30 (Server time)\n\nDuring breaks, or when there are special circumstances, I will post the schedule each week (as soon as I know it) out in the Announcements section.\n\nThere is also a calendar on the Corkboard! [(link)](https://velocitycorkboard.com)",
+                        value: "\n**The raid schedule is always the same during an active Tier**:\n- Tuesday, Thursday, Sunday\n- Invites at 8:15\n- Raid 8:30-10:30 (Server time)\n\n**When there are special circumstances**, such as during a break, I will post the schedule each week (as soon as I know it) out in the #announcements section.\n\nThere is also a calendar on the Corkboard! [(link)](https://velocitycorkboard.com)",
                         inline: true
                     })
 
@@ -136,11 +141,23 @@ module.exports = {
                     .setTitle("How do I sign up to raid?")
                     .addFields({
                         name: " ",
-                        value: "\nRaid signups are handled on the Corkboard through a spreadsheet. You can find it here in the Extras section [(link)](https://velocitycorkboard.com).\n\nNeed the password?  Type `/corkboard` in any public channel. You're the only one that will see the response!",
+                        value: "\nRaid signups are handled on the Corkboard through a spreadsheet. You can find it here in the Extras section [(link)](https://velocitycorkboard.com). If the deadline has already passed, just contact Evie in game or in a Discord message!\n\nNeed the password?  Type `/corkboard` in any public channel. You're the only one that will see the response!",
                         inline: true
                     })
 
                 interaction.reply({ embeds: [signupEmbed], ephemeral: true });
+                break;
+            case 'strats':
+                stratsEmbed = new EmbedBuilder()
+                    .setColor(0xFFFFFF)
+                    .setTitle("How do I find the raid strats?")
+                    .addFields({
+                        name: " ",
+                        value: "\n**Raid strats are in two places!**\n\n- The formal strat is on the Corkboard, and you should read this version first (link)\n- For quick reminders, you can look in the #raid-strats section of Discord, which has the most relevant diagram and a very brief bullet list. **This version will not teach you the strat!**",
+                        inline: true
+                    })
+
+                interaction.reply({ embeds: [stratsEmbed], ephemeral: true });
                 break;
             case 'trb':
                 trbEmbed = new EmbedBuilder()
@@ -148,7 +165,7 @@ module.exports = {
                     .setTitle("How do I join the TRB community channel in game?")
                     .addFields({
                         name: " ",
-                        value: `\nIf you have a new alt and you want to get into the community channel:\n- Open up the Community settings thing in game and select “Join a community”.\n- When prompted for a code, paste in: ${process.env.raidbrigcode}\n\nYou can also access this by typing /raidbrigade into any public channel. Only you will see the result!`,
+                        value: `\n**If you have a new alt and you want to get into the community channel**:\n- Open up the Community settings thing in game\n- Select “Join a community”.\n- Select "The Raid Brigade".\n- When prompted for a code, paste in: ${process.env.raidbrigcode}\n\n*You can also access this by typing /raidbrigade into any public channel. Only you will see the result!*`,
                         inline: true
                     })
 
