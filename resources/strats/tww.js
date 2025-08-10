91c27fc78a46d39db472bd6b875fc5f86e3f1fe1
jdmartin <jonsaccount+githubjd@icloud.com>
2025-08-10 05:36:36 -0700

    Updated Strats

--- resources/strats/tww.js
+++ resources/strats/tww.js
@@ -15,7 +15,7 @@ const manaforge = new EmbedBuilder()
     })
     .addFields({
         name: "Bosses",
-        value: `[Plexus Sentinel](${theCommonStem}/1403034952026685540)\n[Loom'ithar](${theCommonStem}/1403049142539911228)\n[Soulbinder](${theCommonStem}/1403417292389355560)\n`,
+        value: `[Plexus Sentinel](${theCommonStem}/1403034952026685540)\n[Loom'ithar](${theCommonStem}/1403049142539911228)\n[Soulbinder](${theCommonStem}/1403417292389355560)\n[Forgeweaver Araz](${theCommonStem}/1403931594468622368)\n`,
         inline: true,
     })
     .addFields({
