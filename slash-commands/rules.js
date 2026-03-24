bfed5278969869dee25ec605117c680f956fede7
jdmartin <jonsaccount+githubjd@icloud.com>
2026-03-23 19:55:10 -0700

    Updated deps, rules.js

--- slash-commands/rules.js
+++ slash-commands/rules.js
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle, MessageFlags, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags, SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder().setName("rules").setDescription("Rules for Raiding!");
 export async function execute(interaction) {
@@ -9,11 +9,9 @@ export async function execute(interaction) {
 
     const row = new ActionRowBuilder().addComponents(button);
 
-    const file = new AttachmentBuilder("./resources/images/rules.png");
-
     interaction.reply({
-        content: "Here are the raid rules. You can also see the latest version on the Corkboard!",
-        files: [file],
+        content:
+            "Times have changed!\n\nTo see the latest rules, visit [#raider-contract](https://discord.com/channels/308622057707536385/1482542679366696991).\n\nYou can also see the latest version on the Corkboard!",
         components: [row],
         flags: MessageFlags.Ephemeral,
     });
