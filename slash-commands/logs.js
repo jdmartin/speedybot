af17031521b4c6872422f572fd4dfe7c93cab88b
jdmartin <jonsaccount+githubjd@icloud.com>
2026-05-07 03:43:36 -0700

    Make logs ephemeral

--- slash-commands/logs.js
+++ slash-commands/logs.js
@@ -1,6 +1,9 @@
-import { SlashCommandBuilder } from "discord.js";
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder().setName("logs").setDescription("Replies with the guild's raiding logs.");
 export async function execute(interaction) {
-    return interaction.reply("Our logs are here: https://www.warcraftlogs.com/guild/reports-list/41907/");
+    return interaction.reply({
+        content: "Our logs are here: https://www.warcraftlogs.com/guild/reports-list/41907/",
+        flags: MessageFlags.Ephemeral,
+    });
 }
